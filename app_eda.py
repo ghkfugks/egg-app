a86b0b91a0d3d3353447fcd18d4951e052943844
jung hyun <hfghhghg11@gmil.com>
2022-05-26 12:13:51 +0900

    app

--- app_eda.py
+++ app_eda.py
@@ -1,8 +1,41 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sb
 
 
 def run_data():
-    pass
+    df = pd.read_csv('data/egg.csv', index_col= 0)
+    
+
+    if st.checkbox('전체데이터'):
+        st.dataframe(df)
+    else :
+        st.text('상위 5개')
+        st.dataframe(df.head())
+
+    
+    column_list = df.columns
+    
+    choice_list = st.multiselect('보고싶은 데이터 선택', column_list)
+
+    st.dataframe(df[choice_list])
+
+
+    col_list = df.columns[[2,3,4,6,7,8,9] ]
+
+    selected_list = st.multiselect('데이터별 상관계수',col_list)
+    
+
+    if len(selected_list) > 1 :
+        
+
+        st.text('선택하신 컬럼끼리의 상관계수 입니다.')
+        st.dataframe(df[selected_list].corr())
+
+
+        fig2 = plt.figure()
+        sb.heatmap(data = df[selected_list].corr(), annot=True, fmt='.2f',
+                vmin = -1, vmax = 1, cmap = 'coolwarm', linewidths=0.5)
+        st.pyplot(fig2)
+
--- app_ml.py
+++ app_ml.py
@@ -1,14 +1,44 @@
 import streamlit as st
-
-
-import streamlit as st
-
-
-
 import joblib
 import numpy as np
+import sklearn
 
 
 
 def run_ml() :
-    pass
+    st.subheader('닭 조건에 따른 알의 무게')
+
+
+    regressor = joblib.load('data/regressor1.pkl')
+    scaler_X = joblib.load('data/scaler_X1.pkl')
+    scaler_y =joblib.load('data/scaler_y1.pkl')
+
+    variety = st.radio('품종을 선택하세요.',['Marans','Ameraucana'])
+    if variety == 'Marans' :
+        variety = 0
+    else :
+        variety = 1
+
+    age = st.number_input('닭 나이',0,5000)
+    weigth = st.number_input('닭 무게',0,10000)
+    Feed   = st.number_input('사료 양',0,1000)
+
+    PerDay = st.radio('하루에 알을 낳는가?.',['Yes','No'])
+    if PerDay == 'Marans' :
+        PerDay = 0
+    else :
+        PerDay = 1
+
+    SunLight = st.number_input('햇빛 노출 시간',0,24)
+
+    if st.button('달걀 무게 예측'):
+
+        new_data = np.array([variety,age,weigth,Feed,PerDay,SunLight])
+
+        new_data = new_data.reshape(1, 6)
+        new_data = scaler_X.transform(new_data)
+
+        y_pred = regressor.predict(new_data)
+        y_pred = scaler_y.inverse_transform(y_pred)
+
+        st.write('이 달걀 무게는'  + str(y_pred) + 'gram 입니다.')
